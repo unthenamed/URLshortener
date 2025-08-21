202cde5af7e51c64a7da1c567ad10f509c55a66c
Unthenamed <unthenamed@gmail.com>
2025-08-21 16:28:08 +0700

    Update github-redirect.js

--- github-redirect.js
+++ github-redirect.js
@@ -4,7 +4,7 @@
 // Format: https://api.github.com/repos/{owner}/{repo}/issues/
 var GITHUB_ISSUES_LINK =
   "https://api.github.com/repos/eexit/s/issues/";
-var PATH_SEGMENTS_TO_SKIP = 1;
+var PATH_SEGMENTS_TO_SKIP = 0;
 
 /**
  * DO NOT TOUCH ANYTHING BELOW THIS COMMENT UNLESS YOU KNOW WHAT YOU ARE DOING
